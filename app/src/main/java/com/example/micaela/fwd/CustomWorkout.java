b612ad85d9a2a0f0e20f1a4664c4fecfbdaa811c
heggle <hilaryeg@gmail.com>
2018-05-27 22:38:02 -0700

    Created generate button on MainActivity and intent linking it to CustomWorkout Page

---
+++ app/src/main/java/com/example/micaela/fwd/CustomWorkout.java
@@ -0,0 +1,21 @@
+package com.example.micaela.fwd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class CustomWorkout extends AppCompatActivity {
+
+    private TextView mDisplayText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_workout);
+
+        //Showing an example of content passed into page no. 2
+        mDisplayText = (TextView) findViewById(R.id.tv_display);
+    }
+}
--- app/src/main/java/com/example/micaela/fwd/MainActivity.java
+++ app/src/main/java/com/example/micaela/fwd/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.micaela.fwd;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mGenerateWorkout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGenerateWorkout = (Button) findViewById(R.id.generateWorkoutButton);
+
+
+        //Set an on-click listener to do something when generateWorkoutButton clicked
+        mGenerateWorkout.setOnClickListener(new View.OnClickListener() {
+
+
+            // The onClick method is triggered when this button (mDoSomethingCoolButton) is clicked
+            // param v is The view that is clicked. In this case, it's mGenerateWorkout Button.
+
+            @Override
+            public void onClick(View v) {
+
+                //set to context to go in the intent call
+                Context context = MainActivity.this;
+
+                // Store the destination activity in a class to go in the intent call
+                Class destinationActivity = CustomWorkout.class;
+
+                // Create the intent that will be used to start the CustomWorkout Activity -- intent
+                // creation needs a context and a destination
+                Intent startCustomWorkoutActivityIntent = new Intent(context, destinationActivity);
+
+                // Start the CustomWorkout activity
+                startActivity(startCustomWorkoutActivityIntent);
+            }
+        });
     }
 }
+

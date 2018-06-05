29f2b310b2a986451a82a069ee1d091150e2a744
heggle <hilaryeg@gmail.com>
2018-06-05 16:35:34 -0700

    Content of dropdown menus added and visible

--- app/src/main/java/com/example/micaela/fwd/MainActivity.java
+++ app/src/main/java/com/example/micaela/fwd/MainActivity.java
@@ -6,9 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.JsonReader;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.util.Log;
+import android.widget.Spinner;
 
 import com.example.micaela.fwd.CustomWorkout;
 
@@ -16,6 +18,7 @@ import java.io.IOException;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.util.Random;
+import java.util.ArrayList;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,10 +26,13 @@ import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
 public class MainActivity extends AppCompatActivity {
 
+    //Declare the generate workout button
     private Button mGenerateWorkout;
+    //Declare all the dropDown menus
+    private Spinner spinnerWorkoutDuration, spinnerEquipment, spinnerCardioVsStrength,
+        spinnerTargetedMuscles, spinnerFitnessGoal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +41,23 @@ public class MainActivity extends AppCompatActivity {
         mGenerateWorkout = (Button) findViewById(R.id.generateWorkoutButton);
 
 
+        //Initialize Spinner objects and link them to their xml id
+        spinnerWorkoutDuration = (Spinner) findViewById(R.id.spinnerTimeDuration);
+        spinnerEquipment = (Spinner) findViewById(R.id.spinnerEquipment);
+        spinnerCardioVsStrength = (Spinner) findViewById(R.id.spinnerCardioVsStrength);
+        spinnerTargetedMuscles = (Spinner) findViewById(R.id.spinnerTargetedMuscles);
+        spinnerFitnessGoal = (Spinner) findViewById(R.id.spinnerFitnessGoal);
+
+        //For each spinner, call the array adapter
+        createArrayAdapter(R.array.duration_of_workout, spinnerWorkoutDuration); //Right now just calling it for the duration of workout
+        createArrayAdapter(R.array.equipment_available, spinnerEquipment);
+        createArrayAdapter(R.array.type_of_workout, spinnerCardioVsStrength);
+        createArrayAdapter(R.array.muscles_targeted, spinnerTargetedMuscles);
+        createArrayAdapter(R.array.fitness_goal, spinnerFitnessGoal);
+
+
         //Set an on-click listener to do something when generateWorkoutButton clicked
         mGenerateWorkout.setOnClickListener(new View.OnClickListener() {
-
 
             // The onClick method is triggered when this button (mDoSomethingCoolButton) is clicked
             // param v is The view that is clicked. In this case, it's mGenerateWorkout Button.
@@ -60,6 +80,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
+    //Function to Create an array adapter and set adapter
+    public void createArrayAdapter(int textArrayResId, Spinner spinner){
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                textArrayResId, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
 
     public void createWorkout(int duration, boolean equipment, String muscleGroup, String type) {
         // This function takes all of the potential workout exercises and iterates through them and
@@ -207,5 +242,6 @@ public class MainActivity extends AppCompatActivity {
         else
             return false;
     }
+
 }
 

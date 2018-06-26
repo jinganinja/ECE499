e97a094fb72c0bf163bd78640a0bdb048275aab0
DESKTOP-GV2R7CQ\micaela <micaela@19labs.com>
2018-06-26 08:47:57 -0700

    Start working on the algorithm

--- app/src/main/java/com/example/micaela/fwd/MainActivity.java
+++ app/src/main/java/com/example/micaela/fwd/MainActivity.java
@@ -7,6 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.util.Log;
+
+import com.example.micaela.fwd.CustomWorkout;
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,6 +55,64 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(startCustomWorkoutActivityIntent);
             }
         });
+    }
+
+    public void createWorkout(int duration, boolean equipment, String muscleGroup, String type) {
+        // This function takes all of the potential workout exercises and iterates through them and
+        // adds the applicable ones to the current workout potential exercises list. From that list
+        // there will be a random number generator that picks the number of exercises that will be
+        // used for the workout.
+
+        JSONParser parser = new JSONParser();
+        try {
+            JSONArray workoutExercises = (JSONArray) parser.parse(new FileReader("/main/json/exercises.json"));
+            JSONArray allAvailableExercises = (JSONArray) parser.parse(new FileReader("/main/json/exercises.json"));
+            // check equipment to see if we can minimize exercises based on that
+            if (!equipment) {
+                for (Object obj:allAvailableExercises) {
+                    // remove all the exercises that require equipment
+                }
+            }
+            // check the muscle group to minimize the exercises
+            if (muscleGroup != "full body") {
+                for (Object obj:allAvailableExercises) {
+                    // remove exercises that are not for that muscle group
+                }
+            }
+            // check the type to minimize the exercises
+            String userExerciseType;
+            for (Object obj:allAvailableExercises) {
+                JSONObject exercise = (JSONObject) obj;
+                userExerciseType = (String) exercise.get("type");
+                if (userExerciseType != type || !userExerciseType.equals("both")) {
+                    // remove that type of exercise
+                }
+            }
+            // determine how many exercises needed for the duration
+            // determine if we have enough
+            // build a random number generator for that many things and generate a random number
+            // use the random number to add that exercise to the workout
+            // remove that exercise from the list and repeat
+            // complete building the workout
+            // check that we have the right amount of time for the duration given for the workout built
+            // post workout
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return;
+        }
+
+
+    }
+
+    public JSONArray createHIIT(int duration, boolean equipment, String muscleGroup, String type) {
+        // create workout
+        return null;
     }
 }
 

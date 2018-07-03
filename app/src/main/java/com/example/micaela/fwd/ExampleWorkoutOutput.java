e46740ffe519140fbea1e92ed053ce2b6332146b
heggle <hilaryeg@gmail.com>
2018-07-03 15:14:31 -0700

    Added Example workout Output class

---
+++ app/src/main/java/com/example/micaela/fwd/ExampleWorkoutOutput.java
@@ -0,0 +1,76 @@
+/* THIS IS A TEMPORARY FUNCTION CREATED ONLY FOR TESTING PURPOSES TO TEST UI W/O BACKEND
+ *
+ */
+package com.example.micaela.fwd;
+
+import android.content.Context;
+import android.renderscript.ScriptGroup;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.file.FileSystems;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ExampleWorkoutOutput {
+    private JSONArray example = new JSONArray();
+    private static final String TAG = "ExampleWorkoutOutput";
+
+
+    //Constructor
+    public ExampleWorkoutOutput() {
+
+        JSONArray workout = new JSONArray();
+        setExample("Pushups", 3, 11);
+        setExample("Bicep Curls",1, 30);
+        setExample("Burpees", 2, 15);
+        Log.d(TAG, "ExampleWorkoutOutput: "+ this.example.toString());
+    }
+
+
+    public void setExample(String name, int sets, int reps) {
+        this.example = example;
+        JSONObject exercise = new JSONObject();
+        JSONObject steps = new JSONObject();
+        JSONArray description = new JSONArray();
+
+        try {
+            //put name of excercise
+            exercise.put("name", name);
+
+            //put description of exercise
+            steps.put("Step 1", "Do this stuff for step 1 of "+ name + ". Do this stuff for step 1.Do this stuff for step 1. Do this stuff for step 1.Do this stuff for step 1.");
+            steps.put("Step 2", "Do this stuff for step 2 of "+ name + ". Do this stuff for step 2.Do this stuff for step 2. Do this stuff for step 1.Do this stuff for step 2.");
+            steps.put("Step 3", "Do this stuff for step 3 of "+ name + ". Do this stuff for step 3.Do this stuff for step 3. Do this stuff for step 1.Do this stuff for step 3.");
+            description.put(steps);
+            exercise.put("description", description);
+
+            //put reps, sets, and image path for an exercise
+            exercise.put("reps",reps);
+            exercise.put("sets", sets);
+            exercise.put("image", "main/res/drawable/push_up_fig_1.png");
+
+            //Add the exercise to our output array
+            this.example.put(exercise);
+
+        } catch (JSONException e){
+            Log.d(TAG, "onClick: JSON exception!!!");
+        }
+    }
+
+    //Getter
+    public JSONArray getExample() {
+        return this.example;
+    }
+
+
+}

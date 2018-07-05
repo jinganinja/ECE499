38f1e293aaf299067e34810def88bad2605042fb
heggle <hilaryeg@gmail.com>
2018-07-05 15:47:15 -0700

    Added the ExerciseListObject.java and ParseJSONforUI.java files necessary for current list implementation

---
+++ app/src/main/java/com/example/micaela/fwd/ExerciseListObject.java
@@ -0,0 +1,61 @@
+/*Class to hold  objects for list of exercises in CustomWorkout Page*/
+
+package com.example.micaela.fwd;
+
+public class ExerciseListObject {
+    private String name;
+    private String tagLine;
+    private String img;
+    private String description; //ToDo Deal with description as a JSON array (and images associated for each step?
+    private String reps;
+    private String sets;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTagLine() {
+        return tagLine;
+    }
+
+    public void setTagLine(String tagLine) {
+        this.tagLine = tagLine;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getReps() {
+        return reps;
+    }
+
+    public void setReps(String reps) {
+        this.reps = reps;
+    }
+
+    public String getSets() {
+        return sets;
+    }
+
+    public void setSets(String sets) {
+        this.sets = sets;
+    }
+
+}

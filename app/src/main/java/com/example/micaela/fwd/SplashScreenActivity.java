7d8873b3c2438af6b3b0fdca46e8840c193f1d57
heggle <hilaryeg@gmail.com>
2018-07-20 21:12:20 -0700

    Fixed Splash Screen

---
+++ app/src/main/java/com/example/micaela/fwd/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.example.micaela.fwd;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
+
